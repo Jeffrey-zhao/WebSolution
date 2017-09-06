fbcd9c84635eb5084d9542b4a8cf59a591f94876
zhaosw <zhao_shuangwu@163.com>
2017-09-07 00:06:54 +0800

    api solve cors issue

---
+++ WebSolution/WebApi/Controllers/ChargingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Cors;
+using System.Web.Http.Cors;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "http://localhost:27239", headers:"*",methods:"GET,POST,PUT,DELETE")]
+    public class ChargingController : ApiController
+    {
+        [HttpGet]
+        public string GetAllChargingData()
+        {
+            return "Success";
+        }
+    }
+}
